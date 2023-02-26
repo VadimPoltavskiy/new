445ad8cdf5b9029053b986899c53f2c859e69adf
Vadim Poltavskiy <poltavskiy123@yandex.ru>
2023-02-26 23:30:42 +0700

    Added lecture2

---
+++ Lecture2/Example010/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] array ={13,21,37,45,59,61,76,58,95};
+int n = array.Length;
+int find = 37;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index=index + 1;
+    index++;
+}
---
+++ Lecture2/Example011/Program.cs
@@ -0,0 +1,41 @@
+﻿void FillArray(int[] collection)
+{
+    int Length = collection.Length;
+    int index =0;
+    while (index < Length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+int [] array = new int [10];
+int indexOf (int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+        }
+        index ++;
+    }
+    return position;
+}
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int pos = indexOf (array, 4);
+Console.WriteLine(pos);
